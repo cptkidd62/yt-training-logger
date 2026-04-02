cb3017cbadffa0f56eb6c995e2715522c29d323e
cptkidd62 <zelinka15@gmail.com>
2026-04-02 15:34:17 +0200

    add deleting logs

--- frontend/src/components/LogsListItem.vue
+++ frontend/src/components/LogsListItem.vue
@@ -1,5 +1,7 @@
 <script lang="ts">
 import { toLocaleFullString } from '@/helpers/DateTools'
+import { logsRepository } from '@/repositories/repositoryProvider'
+import router from '@/router'
 import type { DisplayableLog } from '@/types/traininglog'
 import { defineComponent, type PropType } from 'vue'
 export default defineComponent({
@@ -12,9 +14,17 @@ export default defineComponent({
   setup() {
     return {
       toLocaleFullString,
+      deleteLog,
     }
   },
 })
+
+function deleteLog(log: DisplayableLog) {
+  if (confirm('Do you really want to delete this training?')) {
+    logsRepository.remove(log.id)
+    router.go(0)
+  }
+}
 </script>
 
 <template>
@@ -23,6 +33,7 @@ export default defineComponent({
       ><h3 class="id">{{ training.trainingTitle }}</h3></RouterLink
     >
     <p>{{ toLocaleFullString(training.date) }}</p>
+    <button @click="deleteLog(training)">Delete log</button>
   </div>
 </template>
 
